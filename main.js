e71a60cf2b8cffaf23b471fd242a05862281b8bb
siddharthpd <sidpd510@gmail.com>
2024-02-29 12:51:38 +0530

    add update of education rows

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let form = document.getElementById('form')
 let tbody = document.getElementById('tbody')
 let add = document.getElementById('add')
 let userId =1;
+let createForm = document.getElementById('create-form')
 let firstName = document.querySelector('.fname')
 let lastName = document.querySelector('.lname')
 let email = document.querySelector('.email') 
@@ -14,6 +15,10 @@ let msgEmail = document.getElementById('msgEmail')
 let msgAddress = document.getElementById('msgAddress')
 let msgDob = document.getElementById('msgDob')
 let msgGraduation = document.getElementById('msgGraduation')
+
+createForm.addEventListener('click', () => {
+    resetEducationRows();
+})
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -44,11 +49,11 @@ let formValidation = () => {
     }
     
     const validateDate = (dateInput, message, minAge) => {
-        const dateValue = new Date(dateInput);
-        const currentYear = new Date().getFullYear;
+        const dateValue = new Date(dateInput.value);
+        const currentYear = new Date().getFullYear();
         const yearDiff = currentYear - dateValue.getFullYear();
-
         if(dateInput.value === '' || yearDiff < minAge){
+            console.log('date error ')
             dateInput.classList.add('is-invalid');
             message.innerHTML=`Age must be at least ${minAge} years!`;
             message.classList.add('text-danger');
@@ -56,20 +61,19 @@ let formValidation = () => {
         }
     }
 
-    let degree = document.querySelectorAll('degree');
-    degree.forEach(element => {
-        element.addEventListener('blur', () => {
-            validateDegree(element, )
-        })
-    })
+    // let degree = document.querySelectorAll('degree');
+    // degree.forEach(element => {
+    //     element.addEventListener('blur', () => {
+    //         validateDegree(element, )
+    //     })
+    // })
 
-    const validateDegree = (element, message) => {
-        if(element.value.trim()===''){
-        message.classList.add('is-invalid')
-        message.classList.add
-        }
-
-    }
+    // const validateDegree = (element, message) => {
+    //     if(element.value.trim()===''){
+    //         message.classList.add('is-invalid')
+    //         message.classList.add
+    //     }
+    // }
     
 
     // validating each input field
@@ -77,7 +81,7 @@ let formValidation = () => {
     validateEmail(msgEmail);
     validateField(address, msgAddress);
     validateDate(dob, msgDob, 18); // Minimum age required: 18 years
-    validateDate(graduation, msgGraduation, 0); // graduation year not in the future
+    validateDate(graduation, msgGraduation, 1); // graduation year not in the future
 
 
 
@@ -111,17 +115,75 @@ let addUser = () => {
         email: email.value,
         address: address.value,
         dob: dob.value,
-        graduation: graduation.value
+        graduation: graduation.value,
+        educations: []
     }
+
+    let educationRows = document.querySelectorAll('#educationTableBody tr');
+    educationRows.forEach(row => {
+        let education = {
+            degree: row.querySelector('.degree').value,
+            college: row.querySelector('.college').value,
+            startYear: row.querySelector('.startYear').value,
+            passoutYear: row.querySelector('.passoutYear').value,
+            percentage: row.querySelector('.percentage').value,
+            backlog: row.querySelector('.backlog').value
+        }
+
+        user.educations.push(education);
+    })
+
     users.push(user);
     // for update 
     renderUsers();
     alert('user created successfully!');
 }
 
+// function for adding a new education row in modal 
+const addEducationRow = ()=> {
+    const educationTableBody = document.getElementById('educationTableBody');
+    const newRow = `
+    <tr>
+        <td class="pb-0">
+            <input type="text" class="form-control degree" placeholder="Degree">
+            <p class="msgDegree fs-6"></p>
+        </td>
+        <td>
+            <input type="text" class="form-control college" placeholder="College">
+            <p class="msgCollege fs-6"></p>
+        </td>
+        <td>
+            <input type="month" class="form-control startYear" placeholder="Start Year">
+            <p class="msgStartYear fs-6"></p>
+        </td>
+        <td>
+            <input type="month" class="form-control passoutYear" placeholder="Passout Year">
+            <p class="msgPassoutYear fs-6"></p>
+        </td>
+        <td>
+            <input type="text" class="form-control percentage" placeholder="Percentage" min="0" max="100" step="0.01">
+            <p class="msgPercentage fs-6"></p>
+        </td>
+        <td>
+            <input type="number" class="form-control backlog" placeholder="Backlog" min="0" max="100">
+            <p class="msgBacklog fs-6"></p>
+        </td>
+        <td>
+            <button type="button" onclick="removeEducationRow(this)" class="btn btn-outline-danger remove-education-btn"><i class="fa-solid fa-minus"></i></button>
+        </td>
+    </tr>
+    `;
+    educationTableBody.insertAdjacentHTML('beforeend', newRow);
+};
+
+const removeEducationRow = (button)=> {
+    const row = button.closest('tr');
+    row.remove();
+};
+
+
 let renderUsers = ()=> {
     resetForm();
-
     return ( tbody.innerHTML= users.map((user,index)=> {
         let { id, firstName, lastName, address, email, dob, graduation} = user;
 
@@ -144,11 +206,12 @@ let renderUsers = ()=> {
             </td>
         </tr>
         `
-    }).join(' '))
+    }).join(' '));
 }
 
 let updateUser = (index) => {
     let currentUser = users[index]
+    console.log(currentUser)
 
     if(currentUser){
 
@@ -159,6 +222,44 @@ let updateUser = (index) => {
         dob.value= currentUser.dob;
         graduation.value= currentUser.graduation;
 
+
+        let educationTableBody = document.getElementById('educationTableBody');
+        educationTableBody.innerHTML=''
+        currentUser.educations.forEach(education => {
+            let {degree, college, startYear, passoutYear, percentage, backlog} = education
+            const newRow = `
+                <tr>
+                    <td class="pb-0">
+                        <input type="text" class="form-control degree" placeholder="Degree" value="${degree}">
+                        <p class="msgDegree fs-6"></p>
+                    </td>
+                    <td>
+                        <input type="text" class="form-control college" placeholder="College" value="${college}">
+                        <p class="msgCollege fs-6"></p>
+                    </td>
+                    <td>
+                        <input type="month" class="form-control startYear" placeholder="Start Year" value="${startYear}">
+                        <p class="msgStartYear fs-6"></p>
+                    </td>
+                    <td>
+                        <input type="month" class="form-control passoutYear" placeholder="Passout Year" value="${passoutYear}">
+                        <p class="msgPassoutYear fs-6"></p>
+                    </td>
+                    <td>
+                        <input type="text" class="form-control percentage" placeholder="Percentage" min="0" max="100" step="0.01" value="${percentage}">
+                        <p class="msgPercentage fs-6"></p>
+                    </td>
+                    <td>
+                        <input type="number" class="form-control backlog" placeholder="Backlog" min="0" max="100" value="${backlog}">
+                        <p class="msgBacklog fs-6"></p>
+                    </td>
+                    <td>
+                        <button type="button" onclick="removeEducationRow(this)" class="btn btn-outline-danger remove-education-btn"><i class="fa-solid fa-minus"></i></button>
+                    </td>
+                </tr>
+            `;
+            educationTableBody.insertAdjacentHTML('beforeend', newRow);
+        });
         // Update the button attribute to indicate it's an update action
         add.setAttribute('data-action', 'update')
         add.setAttribute('data-user-id', currentUser.id)
@@ -168,20 +269,35 @@ let updateUser = (index) => {
 // function to update users in the array or list
 let updateUserInList = () => {
     let userIdToUpdate = add.dataset.userId; // Retrieve the user ID from the button attribute
-
+    console.log('inside updateuserlist')
     // Find the user in the list and update their data
     let index = users.findIndex((user) => user.id == userIdToUpdate);
     if (index !== -1) {
-        users[index] = {
+        let updatedUser = {
             id: userIdToUpdate,
             firstName: firstName.value,
             lastName: lastName.value,
             email: email.value,
             address: address.value,
             dob: dob.value,
-            graduation: graduation.value
+            graduation: graduation.value,
+            educations: []
+            
         };
 
+        let educationRows = document.querySelectorAll('#educationTableBody tr');
+        educationRows.forEach(row => {
+            let education = {  
+                degree: row.querySelector('.degree').value,
+                college: row.querySelector('.college').value,
+                startYear: row.querySelector('.startYear').value,
+                passoutYear: row.querySelector('.passoutYear').value,
+                percentage: row.querySelector('.percentage').value,
+                backlog: row.querySelector('.backlog').value
+            }
+            updatedUser.educations.push(education);
+        })
+        users[index]= updatedUser;
         renderUsers(); // Update the UI
         alert('User updated successfully!');
     }
@@ -229,4 +345,9 @@ let resetForm = () => {
     graduation.value='';
     add.setAttribute('data-action', 'add');
     add.removeAttribute('data-user-id');
+}
+
+let resetEducationRows = () => {
+    const educationTableBody = document.getElementById('educationTableBody');
+    educationTableBody.innerHTML = '';
 }
